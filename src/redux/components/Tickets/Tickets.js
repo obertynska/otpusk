190aac9ae2cc9052dd91fe1891218add4f70fe83
irynao <obertynska11@gmail.com>
2021-07-01 11:35:20 +0300

    first and 2d stages

---
+++ src/redux/components/Tickets/Tickets.js
@@ -0,0 +1,9 @@
+const Login = ()=> {
+    return (
+        <form>
+            <h3>login</h3>
+        </form>
+    )
+}
+
+export default Login
---
+++ src/redux/components/common/Preloader/Preloader.js
@@ -0,0 +1,22 @@
+import s from "./Preloader.module.css"
+
+const Preloader = () => {
+    return (
+        <div className={s.sk_fading_circle}>
+            <div className={`${s.sk_circle}`}></div>
+            <div className={`${s.sk_circle2} ${s.sk_circle}`}></div>
+            <div className={`${s.sk_circle3} ${s.sk_circle}`}></div>
+            <div className={`${s.sk_circle4} ${s.sk_circle}`}></div>
+            <div className={`${s.sk_circle5} ${s.sk_circle}`}></div>
+            <div className={`${s.sk_circle6} ${s.sk_circle}`}></div>
+            <div className={`${s.sk_circle7} ${s.sk_circle}`}></div>
+            <div className={`${s.sk_circle8} ${s.sk_circle}`}></div>
+            <div className={`${s.sk_circle9} ${s.sk_circle}`}></div>
+            <div className={`${s.sk_circle10} ${s.sk_circle}`}></div>
+            <div className={`${s.sk_circle11} ${s.sk_circle}`}></div>
+            <div className={`${s.sk_circle12} ${s.sk_circle}`}></div>
+        </div>
+    )
+}
+
+export default Preloader
