4fbb72121ad903e03d29d80b80350cb9a11b6270
irynao <obertynska11@gmail.com>
2021-07-06 08:54:55 +0300

    clearing uneccessary comments

--- src/redux/authReduser.js
+++ src/redux/authReduser.js
@@ -144,20 +144,13 @@ export const showUserTickets = () => dispatch => {
 
                 return new ReadableStream({
                     start(controller) {
-                        // The following function handles each data chunk
                         function push() {
-                            // "done" is a Boolean and value a "Uint8Array"
                             reader.read().then(({done, value}) => {
-                                // If there is no more data to read
                                 if (done) {
-
                                     controller.close();
                                     return;
                                 }
-                                // Get the data and send it to the browser via the controller
                                 controller.enqueue(value);
-                                // Check chunks by logging to the console
-
                                 push();
                             })
                         }
